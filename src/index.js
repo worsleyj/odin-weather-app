c4b18a6265e4ce351c486cc85c48eeacd279503f
worsleyj <josephworsley23@gmail.com>
2024-08-27 11:42:06 -0400

    add input box for custom location

--- src/index.js
+++ src/index.js
@@ -1,26 +1,30 @@
 import "./reset.css";
 import "./styles.css";
 
-fetch(
-  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/north%20carolina?unitGroup=us&include=current&key=WFBUJE4N5UY2XA5TGQBQC5VK4&contentType=json",
-  {
-    method: "GET",
-    headers: {},
-  }
-)
-  .then((response) => {
-    return response.json();
-  })
-  .then(function (response) {
-    processData(response);
-  })
-  .catch((err) => {
-    console.error(err);
-  });
+const submitBtn = document.querySelector(".submit");
 
-function processData(data) {
-  displayData(data);
-  console.log(data);
+function fetchWeather(location) {
+  fetch(
+    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/north%20carolina?unitGroup=us&include=current&key=WFBUJE4N5UY2XA5TGQBQC5VK4&contentType=json",
+    {
+      method: "GET",
+      headers: {},
+    }
+  )
+    .then((response) => {
+      return response.json();
+    })
+    .then(function (response) {
+      processData(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+
+  function processData(data) {
+    displayData(data);
+    console.log(data);
+  }
 }
 
 function displayData(data) {
@@ -32,12 +36,21 @@ function displayData(data) {
   const humidity = document.querySelector(".humidity");
   const description = document.querySelector(".description");
   const datetime = document.querySelector(".datetime");
+  const location = document.querySelector(".location");
   temp.textContent = data.days[0].temp;
-  tempMin.textContent = data.days[0].tempmin;
-  tempMax.textContent = data.days[0].tempmax;
-  feelsLike.textContent = data.days[0].feelslike;
-  uv.textContent = data.days[0].uvindex;
-  humidity.textContent = data.days[0].humidity;
+  tempMin.textContent = "Min: " + data.days[0].tempmin;
+  tempMax.textContent = "Max: " + data.days[0].tempmax;
+  feelsLike.textContent = "Feels Like " + data.days[0].feelslike;
+  uv.textContent = "UV Index: " + data.days[0].uvindex;
+  humidity.textContent = "Humidity: " + data.days[0].humidity;
   description.textContent = data.days[0].description;
   datetime.textContent = data.days[0].datetime;
+  location.textContent = data.address;
 }
+
+submitBtn.addEventListener("click", () => {
+  const locationInput = document.querySelector(".location-input");
+  console.log(locationInput.value);
+
+  fetchWeather(locationInput.value);
+});
