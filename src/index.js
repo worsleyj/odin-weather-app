fb40ac247d4906f11f837f08f616179e0175b911
worsleyj <josephworsley23@gmail.com>
2024-08-27 11:17:37 -0400

    add api calling function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import "./reset.css";
 import "./styles.css";
 
-console.log("TEST2");
+fetch(
+  "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/north%20carolina?unitGroup=us&include=current&key=WFBUJE4N5UY2XA5TGQBQC5VK4&contentType=json",
+  {
+    method: "GET",
+    headers: {},
+  }
+)
+  .then((response) => {
+    return response.json();
+  })
+  .then(function (response) {
+    processData(response);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+function processData(data) {
+  console.log(data);
+}
+// temp, tempmax, tempmin, windspeed, uvindex, humidity, feelslike, description, datetime
